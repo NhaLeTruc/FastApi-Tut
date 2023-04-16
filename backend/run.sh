78657d621455cc73e9ea46b1203f9c0bf039afe5
Nha Le Truc <nha.letruc@gmail.com>
2023-04-16 22:34:52 +0700

    Deploy to Heroku #1

---
+++ backend/run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+PRE_START_PATH=${PRE_START_PATH:-/backend/prestart.sh}
+echo "Checking for script in $PRE_START_PATH"
+if [ -f $PRE_START_PATH ] ; then
+    echo "Running script $PRE_START_PATH"
+    . "$PRE_START_PATH"
+else
+    echo "There is no script $PRE_START_PATH"
+fi
+
+export APP_MODULE=${APP_MODULE-app.main:backend}
+export HOST=${HOST:-0.0.0.0}
+export PORT=${PORT:-8000}
+export BACKEND_CORS_ORIGINS=${BACKEND_CORS_ORIGINS}
+
+
+# run gunicorn
+exec gunicorn --bind $HOST:$PORT "$APP_MODULE" -k uvicorn.workers.UvicornWorker
